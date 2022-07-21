5b2e676a36a5171e7becead8a760516d52608f99
Hadi <hadiforooghi1377@gmail.com>
2022-07-21 16:53:58 +0430

    JWT authentication

--- app/api/api_v1/router.py
+++ app/api/api_v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.api.api_v1.handlers.user import user_router
-
+from app.api.auth.jwt import auth_router
 router = APIRouter()
 
 router.include_router(router=user_router, prefix='/users',tags=["users"])
+router.include_router(router=auth_router, prefix='/auth',tags=["auth"])
